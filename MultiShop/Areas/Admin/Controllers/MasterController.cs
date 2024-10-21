b4ce8a8f095f1b5967da8f00fef537093e2c9d50
Phu Vu Phan Quang <phu.vuphanquang@gmail.com>
2024-10-21 17:58:48 +0700

    initial commit

---
+++ MultiShop/Areas/Admin/Controllers/MasterController.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using MultiShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.ApplicationServices;
+using System.Web.Mvc;
+
+namespace MultiShop.Areas.Admin.Controllers
+{
+    public class MasterController : SecurityController
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Users = sdb.Users.Where(u => u.Roles.Count > 0);
+            ViewBag.Roles = sdb.Roles;
+            
+            ViewBag.OldRoleName = new SelectList(sdb.Roles, "Name", "Name");
+            ViewBag.CurrentUserName = User.Identity.Name;
+
+            var currentUser = UserManager.FindById(User.Identity.GetUserId());
+            var userRoles = UserManager.GetRoles(currentUser.Id);
+
+            ViewBag.CurrentUserRoles = userRoles;
+            return View();
+        }
+
+        public ActionResult AddRole(String NewRoleName)
+        {
+            var role = new IdentityRole();
+            role.Name = NewRoleName;
+            sdb.Roles.Add(role);
+            sdb.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult RemoveRole(String OldRoleName)
+        {
+            var role = sdb.Roles.Single(r => r.Name == OldRoleName);
+            sdb.Roles.Remove(role);
+            sdb.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        /*
+         * USER
+         */
+        public ActionResult AddUser(String UserName, String Password, String[] Roles)
+        {
+            var user = new ApplicationUser();
+            user.UserName = UserName;
+
+            var result = UserManager.Create(user, Password);
+
+            if (result.Succeeded)
+            {
+                foreach (var role in Roles)
+                {
+                    UserManager.AddToRole(user.Id, role);
+                }
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult RemoveUser(String UserName)
+        {
+            var user = UserManager.FindByName(UserName);
+
+            foreach (var ur in user.Roles.ToList())
+            {
+                UserManager.RemoveFromRole(user.Id, ur.Role.Name);
+            }
+
+            sdb.Users.Remove(user);
+
+            sdb.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult UpdateRole(String Name, bool Status, String UserName)
+        {
+            if (string.IsNullOrEmpty(UserName))
+            {
+                return Content("UserName is null or empty.");
+            }
+
+            var user = UserManager.FindByName(UserName);
+            if (user == null)
+            {
+                return HttpNotFound("User not found.");
+            }
+
+            if (Status)
+            {
+                UserManager.AddToRole(user.Id, Name);
+            }
+            else
+            {
+                UserManager.RemoveFromRole(user.Id, Name);
+            }
+            return Content("Update successfully!");
+        }
+    }
+}
